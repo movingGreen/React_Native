a3bdfa61089a5ef8a64c2d8f17a4aaab09846f13
Elizeu <el_vailant@hotmail.com>
2023-05-10 14:31:14 -0400

    outros testes

--- EstudoRN/App.js
+++ EstudoRN/App.js
@@ -1,36 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react'
-import { WebView } from "react-native-webview";
-import html_script from './html_script';
+import React, { useState, useEffect } from 'react'
+import { MapView, UrlTile} from 'react-native-maps';
 
 import * as Location from 'expo-location';
 
-
-async function getLocation() {
-  const { status } = await Location.requestForegroundPermissionsAsync();
-  if (status !== 'granted') {
-    alert('Permission to access location was denied');
-    return;
-  }
-
-  const location = await Location.getCurrentPositionAsync({});
-  console.log(location);
-}
-
-
-
-
 export default function App() {
-  const [vezesClicado, setVezesClicado] = useState(0)
-  getLocation()
+  const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
 
-  
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setPosition({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      });
+    })();
+  }, []);
+
   return (
     <>
-      <View style={styles.container}>
+      <View >
         <Text>Clique no botão e veja o número aumentar :)</Text>
-        <Text>O botão foi clicado {vezesClicado} vezes... </Text>
         <Button 
           onPress={() => {
             setVezesClicado(vezesClicado + 1);
@@ -41,17 +44,27 @@ export default function App() {
           title='próxima tela'
         />
       </View>
-      <WebView source={{html: html_script}} style={{ flex: 1 }} />
+      <MapView region={position} >
+        <UrlTile
+          /**
+           * The url template of the tile server. The patterns {x} {y} {z} will be replaced at runtime
+           * For example, http://c.tile.openstreetmap.org/{z}/{x}/{y}.png
+           */
+          // urlTemplate={"http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+          /**
+           * The maximum zoom level for this tile overlay. Corresponds to the maximumZ setting in
+           * MKTileOverlay. iOS only.
+           */
+          maximumZ={19}
+          /**
+           * flipY allows tiles with inverted y coordinates (origin at bottom left of map)
+           * to be used. Its default value is false.
+           */
+          flipY={false}
+        />
+      </MapView>;
       
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
